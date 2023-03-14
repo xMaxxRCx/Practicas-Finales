5dc4c298608413c51f7223da0881fa7ac1357c07
Fernando-R-C <107069445+xMaxxRCx@users.noreply.github.com>
2023-03-14 13:14:28 -0600

    index
    index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <script src="js/jquery.js"></script>
+    <title>Practica1 con php</title>
+</head>
+<body>
+    <script src="js/bootstrap.min.js" ></script>
+    <!-- navbar -->
+    <?php include "include/navbar.php" ?>
+    <?php  include "include/menu.php"?>
+    
+</body>
+</html>
