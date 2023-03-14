bcc8b47f8fc75805c87ad237be17295aaf83ded1
Fernando-R-C <107069445+xMaxxRCx@users.noreply.github.com>
2023-03-14 13:14:03 -0600

    include
    include

---
+++ include/navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <div class="container-fluid">
+         <a class="btn btn-info" data-bs-toggle="offcanvas" href="#offcanvasExample">Menu</a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">  
+        <li class="nav-item">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Marketing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Finanzas</a></li>
+            <li><a class="dropdown-item" href="#">Conocenos</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Desabilitado</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-danger       " type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
